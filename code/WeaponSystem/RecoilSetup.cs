418e347a07528f25fab9048079c89764718f66ce
Tony <devultj@gmail.com>
2022-09-02 12:32:29 -0400

    Simple, weapon-contained recoil steup

---
+++ code/WeaponSystem/RecoilSetup.cs
@@ -0,0 +1,8 @@
+namespace Facepunch.Gunfight;
+
+public struct RecoilSetup
+{
+	public Vector2 Decay { get; set; }
+	public Vector2 BaseRecoilMinimum { get; set; }
+	public Vector2 BaseRecoilMaximum { get; set; }
+}

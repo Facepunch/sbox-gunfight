9fb124633a5a2c2f001861dcb13fec18d4202b65
Tony <devultj@gmail.com>
2022-09-02 13:59:35 -0400

    Add ViewModel recoil with separate scaling and recovery factor

--- code/WeaponSystem/RecoilSetup.cs
+++ code/WeaponSystem/RecoilSetup.cs
@@ -5,4 +5,6 @@ public struct RecoilSetup
 	public Vector2 Decay { get; set; }
 	public Vector2 BaseRecoilMinimum { get; set; }
 	public Vector2 BaseRecoilMaximum { get; set; }
+	public float ViewModelScale { get; set; }
+	public float ViewModelRecoverySpeed { get; set; }
 }

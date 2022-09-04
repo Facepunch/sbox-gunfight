2b9ce29a5724aa966e0cc75fbccf48673acd8db0
Tony <devultj@gmail.com>
2022-09-03 22:07:43 -0400

    Add CameraModifier, ScreenShake.Pitch

---
+++ code/Player/CameraModifier/Pitch.cs
@@ -0,0 +1,25 @@
+namespace Facepunch.Gunfight.ScreenShake;
+
+public class Pitch : CameraModifier
+{
+	float Length = 5.0f;
+	float Size = 1.0f;
+
+	TimeSince lifeTime = 0;
+
+	public Pitch( float length = 1.5f, float size = 1.0f )
+	{
+		Length = length;
+		Size = size;
+	}
+
+	public override bool Update( ref CameraSetup cam )
+	{
+		var delta = ((float)lifeTime).LerpInverse( 0, Length, true );
+		delta = Easing.EaseOut( delta );
+
+		cam.Rotation *= Rotation.From( new( Size * ( 1 - delta ), 0, 0 ) );
+
+		return lifeTime < Length;
+	}
+}

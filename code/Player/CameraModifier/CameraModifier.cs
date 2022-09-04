54adb2f6a57cae3b2e4019dc59fe5e4b74a105d5
Tony <devultj@gmail.com>
2022-09-03 22:07:43 -0400

    Add CameraModifier, ScreenShake.Pitch

---
+++ code/Player/CameraModifier/CameraModifier.cs
@@ -0,0 +1,33 @@
+namespace Facepunch.Gunfight;
+
+public abstract class CameraModifier
+{
+	internal static List<CameraModifier> List = new();
+
+	internal static void Apply( ref CameraSetup setup )
+	{
+		for ( int i = List.Count; i > 0; i-- )
+		{
+			var keep = List[i - 1].Update( ref setup );
+
+			if ( !keep )
+				List.RemoveAt( i - 1 );
+		}
+	}
+
+	public static void ClearAll()
+	{
+		List.Clear();
+	}
+
+	public CameraModifier()
+	{
+		if ( Prediction.FirstTime )
+		{
+			List.Add( this );
+		}
+	}
+
+	public abstract bool Update( ref CameraSetup setup );
+
+}

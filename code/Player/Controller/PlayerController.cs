34f9abe67b43de22a6abb7df78e2a0ffb4ceb83a
Tony <devultj@gmail.com>
2022-09-03 21:02:31 -0400

    Start killing player controller

--- code/Player/Controller/PlayerController.cs
+++ code/Player/Controller/PlayerController.cs
@@ -9,14 +9,9 @@ public partial class PlayerController : BasePlayerController
 	[Net] public float DefaultSpeed { get; set; } = 220.0f;
 	[Net] public float Acceleration { get; set; } = 10.0f;
 	[Net] public float AirAcceleration { get; set; } = 5.0f;
-	[Net] public float FallSoundZ { get; set; } = -30.0f;
 	[Net] public float GroundFriction { get; set; } = 4.0f;
 	[Net] public float StopSpeed { get; set; } = 100.0f;
-	[Net] public float Size { get; set; } = 20.0f;
-	[Net] public float DistEpsilon { get; set; } = 0.03125f;
 	[Net] public float GroundAngle { get; set; } = 46.0f;
-	[Net] public float Bounce { get; set; } = 0.0f;
-	[Net] public float MoveFriction { get; set; } = 1.0f;
 	[Net] public float StepSize { get; set; } = 18.0f;
 	[Net] public float MaxNonJumpVelocity { get; set; } = 140.0f;
 	[Net] public float BodyGirth { get; set; } = 32.0f;
@@ -24,16 +19,14 @@ public partial class PlayerController : BasePlayerController
 	[Net] public float EyeHeight { get; set; } = 64.0f;
 	[Net] public float Gravity { get; set; } = 800.0f;
 	[Net] public float AirControl { get; set; } = 30.0f;
-	public bool Swimming { get; set; } = false;
-	[Net] public bool AutoJump { get; set; } = false;
+
+	[Net, Predicted] public bool IsAiming { get; set; }
+	[Net, Predicted] public bool Swimming { get; set; }
+	[Net, Predicted] public bool cachedSprint { get; set; }
+	[Net, Predicted] public TimeSince SinceStoppedSprinting { get; set; }
 
 	public Duck Duck;
 	public Unstuck Unstuck;
-
-	[Net, Predicted] public bool IsAiming { get; protected set; }
-
-	[Net, Predicted] public bool cachedSprint { get; set; }
-	[Net, Predicted] public TimeSince SinceStoppedSprinting { get; set; }
 
 	public PlayerController()
 	{
@@ -78,7 +71,6 @@ public partial class PlayerController : BasePlayerController
 	public virtual void UpdateBBox()
 	{
 		var girth = BodyGirth * 0.5f;
-
 		var mins = new Vector3( -girth, -girth, 0 ) * Pawn.Scale;
 		var maxs = new Vector3( +girth, +girth, BodyHeight ) * Pawn.Scale;
 
@@ -93,7 +85,6 @@ public partial class PlayerController : BasePlayerController
 	public override void FrameSimulate()
 	{
 		base.FrameSimulate();
-
 		EyeRotation = Input.Rotation;
 	}
 
@@ -116,29 +107,8 @@ public partial class PlayerController : BasePlayerController
 		var weapon = player.ActiveChild as GunfightWeapon;
 		IsAiming = wantsToAim && !IsSprinting && !(weapon?.IsReloading ?? false);
 
-		RestoreGroundPos();
-
-		//Velocity += BaseVelocity * ( 1 + Time.Delta * 0.5f );
-		//BaseVelocity = Vector3.Zero;
-
-		//Rot = Rotation.LookAt( Input.Rotation.Forward.WithZ( 0 ), Vector3.Up );
-
 		if ( Unstuck.TestAndFix() )
 			return;
-
-		// Check Stuck
-		// Unstuck - or return if stuck
-
-		// Set Ground Entity to null if  falling faster then 250
-
-		// store water level to compare later
-
-		// if not on ground, store fall velocity
-
-		// player->UpdateStepSound( player->m_pSurfaceData, mv->GetAbsOrigin(), mv->m_vecVelocity )
-
-
-		// RunLadderMode
 
 		CheckLadder();
 		Swimming = Pawn.WaterLevel > 0.6f;
@@ -154,21 +124,7 @@ public partial class PlayerController : BasePlayerController
 			BaseVelocity = BaseVelocity.WithZ( 0 );
 		}
 
-
-		/*
-             if (player->m_flWaterJumpTime)
-            {
-	            WaterJump();
-	            TryPlayerMove();
-	            // See if we are still in water?
-	            CheckWater();
-	            return;
-            }
-            */
-
-		// if ( underwater ) do underwater movement
-
-		if ( AutoJump ? Input.Down( InputButton.Jump ) : Input.Pressed( InputButton.Jump ) )
+		if ( Input.Pressed( InputButton.Jump ) )
 		{
 			CheckJumpButton();
 		}
@@ -176,13 +132,9 @@ public partial class PlayerController : BasePlayerController
 		// Fricion is handled before we add in any base velocity. That way, if we are on a conveyor,
 		//  we don't slow when standing still, relative to the conveyor.
 		bool bStartOnGround = GroundEntity != null;
-		//bool bDropSound = false;
 		if ( bStartOnGround )
 		{
-			//if ( Velocity.z < FallSoundZ ) bDropSound = true;
-
 			Velocity = Velocity.WithZ( 0 );
-			//player->m_Local.m_flFallVelocity = 0.0f;
 
 			if ( GroundEntity != null )
 			{
@@ -252,13 +204,6 @@ public partial class PlayerController : BasePlayerController
 			Velocity = Velocity.WithZ( 0 );
 		}
 
-		// CheckFalling(); // fall damage etc
-
-		// Land Sound
-		// Swim Sounds
-
-		SaveGroundPos();
-
 		if ( Debug )
 		{
 			DebugOverlay.Box( Position + TraceOffset, mins, maxs, Color.Red );
@@ -274,7 +219,6 @@ public partial class PlayerController : BasePlayerController
 			DebugOverlay.ScreenText( $" SurfaceFriction: {SurfaceFriction}", lineOffset + 4 );
 			DebugOverlay.ScreenText( $"    WishVelocity: {WishVelocity}", lineOffset + 5 );
 		}
-
 	}
 
 	public virtual bool WishSprinting => Input.Down( InputButton.Run ) && Input.Forward >= 0f;
@@ -302,14 +246,6 @@ public partial class PlayerController : BasePlayerController
 		Velocity = Velocity.WithZ( 0 );
 		Accelerate( wishdir, wishspeed, 0, Acceleration );
 		Velocity = Velocity.WithZ( 0 );
-
-		//   Player.SetAnimParam( "forward", Input.Forward );
-		//   Player.SetAnimParam( "sideward", Input.Right );
-		//   Player.SetAnimParam( "wishspeed", wishspeed );
-		//    Player.SetAnimParam( "walkspeed_scale", 2.0f / 190.0f );
-		//   Player.SetAnimParam( "runspeed_scale", 2.0f / 320.0f );
-
-		//  DebugOverlay.Text( 0, Pos + Vector3.Up * 100, $"forward: {Input.Forward}\nsideward: {Input.Right}" );
 
 		// Add in any base velocity to the current velocity.
 		Velocity += BaseVelocity;
@@ -339,7 +275,7 @@ public partial class PlayerController : BasePlayerController
 		finally
 		{
 
-			// Now pull the base velocity back out.   Base velocity is set if you are on a moving object, like a conveyor (or maybe another monster?)
+			// Now pull the base velocity back out. Base velocity is set if you are on a moving object, like a conveyor (or maybe another monster?)
 			Velocity -= BaseVelocity;
 		}
 
@@ -351,7 +287,6 @@ public partial class PlayerController : BasePlayerController
 		MoveHelper mover = new MoveHelper( Position, Velocity );
 		mover.Trace = mover.Trace.Size( mins, maxs ).Ignore( Pawn );
 		mover.MaxStandableAngle = GroundAngle;
-
 		mover.TryMoveWithStep( Time.Delta, StepSize );
 
 		Position = mover.Position;
@@ -375,11 +310,6 @@ public partial class PlayerController : BasePlayerController
 	/// </summary>
 	public virtual void Accelerate( Vector3 wishdir, float wishspeed, float speedLimit, float acceleration )
 	{
-		// This gets overridden because some games (CSPort) want to allow dead (observer) players
-		// to be able to move around.
-		// if ( !CanAccelerate() )
-		//     return;
-
 		if ( speedLimit > 0 && wishspeed > speedLimit )
 			wishspeed = speedLimit;
 
@@ -408,13 +338,6 @@ public partial class PlayerController : BasePlayerController
 	/// </summary>
 	public virtual void ApplyFriction( float frictionAmount = 1.0f )
 	{
-		// If we are in water jump cycle, don't apply friction
-		//if ( player->m_flWaterJumpTime )
-		//   return;
-
-		// Not on ground - no friction
-
-
 		// Calculate speed
 		var speed = Velocity.Length;
 		if ( speed < 0.1f ) return;
@@ -435,43 +358,16 @@ public partial class PlayerController : BasePlayerController
 			newspeed /= speed;
 			Velocity *= newspeed;
 		}
-
-		// mv->m_outWishVel -= (1.f-newspeed) * mv->m_vecVelocity;
 	}
 
 	public virtual void CheckJumpButton()
 	{
-		//if ( !player->CanJump() )
-		//    return false;
-
-
-		/*
-            if ( player->m_flWaterJumpTime )
-            {
-                player->m_flWaterJumpTime -= gpGlobals->frametime();
-                if ( player->m_flWaterJumpTime < 0 )
-                    player->m_flWaterJumpTime = 0;
-
-                return false;
-            }*/
-
-
-
 		// If we are in the water most of the way...
 		if ( Swimming )
 		{
 			// swimming, not jumping
 			ClearGroundEntity();
-
 			Velocity = Velocity.WithZ( 100 );
-
-			// play swimming sound
-			//  if ( player->m_flSwimSoundTime <= 0 )
-			{
-				// Don't play sound again for 1 second
-				//   player->m_flSwimSoundTime = 1000;
-				//   PlaySwimSound();
-			}
 
 			return;
 		}
@@ -479,48 +375,20 @@ public partial class PlayerController : BasePlayerController
 		if ( GroundEntity == null )
 			return;
 
-		/*
-            if ( player->m_Local.m_bDucking && (player->GetFlags() & FL_DUCKING) )
-                return false;
-            */
-
-		/*
-            // Still updating the eye position.
-            if ( player->m_Local.m_nDuckJumpTimeMsecs > 0u )
-                return false;
-            */
-
 		ClearGroundEntity();
 
-		// player->PlayStepSound( (Vector &)mv->GetAbsOrigin(), player->m_pSurfaceData, 1.0, true );
-
-		// MoveHelper()->PlayerSetAnimation( PLAYER_JUMP );
-
+		// TODO - Look at these bad boys... find out what they do and remove the magic.
 		float flGroundFactor = 1.0f;
-		//if ( player->m_pSurfaceData )
-		{
-			//   flGroundFactor = g_pPhysicsQuery->GetGameSurfaceproperties( player->m_pSurfaceData )->m_flJumpFactor;
-		}
-
 		float flMul = 268.3281572999747f * 1.2f;
-
 		float startz = Velocity.z;
 
 		if ( Duck.IsActive )
 			flMul *= 0.2f;
 
 		Velocity = Velocity.WithZ( startz + flMul * flGroundFactor );
-
 		Velocity -= new Vector3( 0, 0, Gravity * 0.5f ) * Time.Delta;
 
-		// mv->m_outJumpVel.z += mv->m_vecVelocity[2] - startz;
-		// mv->m_outStepHeight += 0.15f;
-
-		// don't jump again until released
-		//mv->m_nOldButtons |= IN_JUMP;
-
 		AddEvent( "jump" );
-
 	}
 
 	public virtual void AirMove()
@@ -531,9 +399,7 @@ public partial class PlayerController : BasePlayerController
 		Accelerate( wishdir, wishspeed, AirControl, AirAcceleration );
 
 		Velocity += BaseVelocity;
-
 		Move();
-
 		Velocity -= BaseVelocity;
 	}
 
@@ -541,15 +407,12 @@ public partial class PlayerController : BasePlayerController
 	{
 		var wishdir = WishVelocity.Normal;
 		var wishspeed = WishVelocity.Length;
-
 		wishspeed *= 0.8f;
 
 		Accelerate( wishdir, wishspeed, 100, Acceleration );
 
 		Velocity += BaseVelocity;
-
 		Move();
-
 		Velocity -= BaseVelocity;
 	}
 
@@ -628,8 +491,6 @@ public partial class PlayerController : BasePlayerController
 		//  Shooting up really fast.  Definitely not on ground trimed until ladder shit
 		//
 		bool bMovingUpRapidly = Velocity.z > MaxNonJumpVelocity;
-		bool bMovingUp = Velocity.z > 0;
-
 		bool bMoveToEndPos = false;
 
 		if ( GroundEntity != null ) // and not underwater
@@ -684,29 +545,12 @@ public partial class PlayerController : BasePlayerController
 		SurfaceFriction = tr.Surface.Friction * 1.25f;
 		if ( SurfaceFriction > 1 ) SurfaceFriction = 1;
 
-		//if ( tr.Entity == GroundEntity ) return;
-
-		Vector3 oldGroundVelocity = default;
-		if ( GroundEntity != null ) oldGroundVelocity = GroundEntity.Velocity;
-
-		bool wasOffGround = GroundEntity == null;
-
 		GroundEntity = tr.Entity;
 
 		if ( GroundEntity != null )
 		{
 			BaseVelocity = GroundEntity.Velocity;
 		}
-
-		/*
-              	m_vecGroundUp = pm.m_vHitNormal;
-            player->m_surfaceProps = pm.m_pSurfaceProperties->GetNameHash();
-            player->m_pSurfaceData = pm.m_pSurfaceProperties;
-            const CPhysSurfaceProperties *pProp = pm.m_pSurfaceProperties;
-
-            const CGameSurfaceProperties *pGameProps = g_pPhysicsQuery->GetGameSurfaceproperties( pProp );
-            player->m_chTextureType = (int8)pGameProps->m_nLegacyGameMaterial;
-            */
 	}
 
 	/// <summary>
@@ -751,28 +595,6 @@ public partial class PlayerController : BasePlayerController
 		if ( trace.StartedSolid ) return;
 		if ( Vector3.GetAngle( Vector3.Up, trace.Normal ) > GroundAngle ) return;
 
-		// This is incredibly hacky. The real problem is that trace returning that strange value we can't network over.
-		// float flDelta = fabs( mv->GetAbsOrigin().z - trace.m_vEndPos.z );
-		// if ( flDelta > 0.5f * DIST_EPSILON )
-
 		Position = trace.EndPosition;
 	}
-
-	void RestoreGroundPos()
-	{
-		if ( GroundEntity == null || GroundEntity.IsWorld )
-			return;
-
-		//var Position = GroundEntity.Transform.ToWorld( GroundTransform );
-		//Pos = Position.Position;
-	}
-
-	void SaveGroundPos()
-	{
-		if ( GroundEntity == null || GroundEntity.IsWorld )
-			return;
-
-		//GroundTransform = GroundEntity.Transform.ToLocal( new Transform( Pos, Rot ) );
-	}
-
 }

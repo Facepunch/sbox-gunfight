b525319f28410a98ed99eb12e94407ea1768407a
bakscratch <lt_8@hotmail.co.uk>
2022-09-08 14:13:12 +0100

    UI tweaks

--- code/UI/Vitals.cs
+++ code/UI/Vitals.cs
@@ -6,20 +6,22 @@ namespace Facepunch.Gunfight;
 public class HealthHud : Panel
 {
 	public IconPanel Icon;
-	public Label Value;
+	public HudBar Value;
 
 	public HealthHud()
 	{
 		Icon = Add.Icon( "add_box", "icon" );
-		Value = Add.Label( "0", "label" );
+		Value = AddChild<HudBar>( "health" );
 	}
 
 	public override void Tick()
 	{
 		var player = Local.Pawn as GunfightPlayer;
 		if ( player == null ) return;
-
-		Value.Text = $"{player.Health.CeilToInt()}";
+		
+		Value.Text.Text = $"{player.Health.CeilToInt()}";
+		Value.InnerBar.Style.Width = Length.Fraction( Math.Max( player.Health / player.MaxHealth, 0.05f ) );
+		Value.InnerBar.Style.Dirty();
 
 		SetClass( "low", player.Health < 40.0f );
 		SetClass( "empty", player.Health <= 0.0f );
@@ -29,13 +31,12 @@ public class HealthHud : Panel
 public class ArmourHud : Panel
 {
 	public IconPanel Icon;
-	public Label Value;
+	public HudBar Value;
 
 	public ArmourHud()
 	{
 		Icon = Add.Icon( "shield", "icon" );
-		Value = Add.Label( "0", "label" );
-
+		Value = AddChild<HudBar>( "armour" );
 	}
 
 	public override void Tick()
@@ -43,7 +44,9 @@ public class ArmourHud : Panel
 		var player = Local.Pawn as GunfightPlayer;
 		if ( player == null ) return;
 
-		Value.Text = $"{player.Armour.CeilToInt()}";
+		Value.Text.Text = $"{player.Armour.CeilToInt()}";
+		Value.InnerBar.Style.Width = Length.Fraction( Math.Max( player.Armour / 100, 0.05f ) );
+		Value.InnerBar.Style.Dirty();
 
 		SetClass( "low", player.Armour < 40.0f );
 		SetClass( "empty", player.Armour <= 0.0f );

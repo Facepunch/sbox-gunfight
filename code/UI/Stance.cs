c02fa0cf2c6bc64537fbf8e56377151c5ad1d1ae
bakscratch <lt_8@hotmail.co.uk>
2022-09-12 15:11:56 +0100

    Temp grenade hud and stance hud

---
+++ code/UI/Stance.cs
@@ -0,0 +1,35 @@
+﻿using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Facepunch.Gunfight;
+
+public class Stance : Panel
+{
+	public Image Icon;
+	
+	protected GunfightPlayer Player => Local.Pawn as GunfightPlayer;
+	protected PlayerController PlayerController => Player?.Controller as PlayerController;
+	
+	public Stance()
+	{
+		Icon = Add.Image( "ui/stance/stand.png", "icon" );
+	}
+
+	public override void Tick()
+	{
+		var player = Local.Pawn as GunfightPlayer;
+		if ( player == null ) return;
+		if ( PlayerController.Duck.IsActive )
+		{
+			Icon.SetTexture( "ui/stance/duck.png" );
+		}
+		else if ( PlayerController.IsSprinting )
+		{
+			Icon.SetTexture( "ui/stance/run.png" );
+		}
+		else
+		{
+			Icon.SetTexture( "ui/stance/stand.png" );
+		}
+	}
+}

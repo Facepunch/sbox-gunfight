5b60673ed79e41cfafd446d2b436d4cabef01b66
bakscratch <lt_8@hotmail.co.uk>
2022-09-12 15:11:56 +0100

    Temp grenade hud and stance hud

---
+++ code/UI/Grenades.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Facepunch.Gunfight;
+
+public class Grenades : Panel
+{
+	public Image Icon;
+	public Label Inventory;
+	public Panel GrenadeBar;
+	public Grenades()
+	{
+		GrenadeBar = Add.Panel( "grenadebar" );
+		Inventory = Add.Label( "[1]", "inventory" );
+		Icon = Add.Image( "ui/weapons/grenade.png", "icon" );
+	}
+
+	int weaponHash;
+
+	public override void Tick()
+	{
+		var player = Local.Pawn as Player;
+		if ( player == null ) return;
+	}
+}

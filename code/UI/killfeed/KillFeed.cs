562e646d67af1c3760fa88930d67566040631a86
Tony <devultj@gmail.com>
2022-09-12 20:59:50 -0400

    Kill Feed will try to use team colors

--- code/UI/killfeed/KillFeed.cs
+++ code/UI/killfeed/KillFeed.cs
@@ -13,6 +13,11 @@ public partial class KillFeed : Panel
 		StyleSheet.Load( "/ui/killfeed/KillFeed.scss" );
 	}
 
+	protected Client GetClient( long steamId )
+	{
+		return Client.All.FirstOrDefault( x => x.PlayerId == steamId );
+	}
+
 	public virtual Panel AddEntry( long lsteamid, string left, long rsteamid, string right, string method )
 	{
 		var e = Current.AddChild<KillFeedEntry>();
@@ -32,6 +37,20 @@ public partial class KillFeed : Panel
 
 		e.Right.Text = right;
 		e.Right.SetClass( "me", rsteamid == Local.PlayerId );
+
+		if ( lsteamid != 0 )
+		{
+			var leftFriendState = TeamSystem.GetFriendState( GetClient( lsteamid ), TeamSystem.MyTeam );
+			e.Left.SetClass( "friendly", leftFriendState == TeamSystem.FriendlyStatus.Friendly );
+			e.Left.SetClass( "enemy", leftFriendState == TeamSystem.FriendlyStatus.Hostile );
+		}
+
+		if ( rsteamid != 0 )
+		{
+			var rightFriendState = TeamSystem.GetFriendState( GetClient( rsteamid ), TeamSystem.MyTeam );
+			e.Right.SetClass( "friendly", rightFriendState == TeamSystem.FriendlyStatus.Friendly );
+			e.Right.SetClass( "enemy", rightFriendState == TeamSystem.FriendlyStatus.Hostile );
+		}
 
 		return e;
 	}

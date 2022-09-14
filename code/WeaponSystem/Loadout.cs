2cce0f3e458c1ceb5a129ba129b6df23802c60f6
Tony <devultj@gmail.com>
2022-09-14 09:44:37 -0400

    Define Loadout GameResource

---
+++ code/WeaponSystem/Loadout.cs
@@ -0,0 +1,54 @@
+namespace Facepunch.Gunfight;
+
+[GameResource( "Gunfight Loadout", "ldt", "A loadout resource for Gunfight" )]
+public partial class Loadout : GameResource
+{
+	public static List<Loadout> All = new();
+
+	/// <summary>
+	/// Loadouts can have tags on them, which can be queried by gamemodes.
+	/// </summary>
+	public List<string> Tags { get; set; }
+
+	// Data
+	public WeaponDefinition PrimaryWeapon { get; set; }
+	public WeaponDefinition SecondaryWeapon { get; set; }
+	public WeaponDefinition MeleeWeapon { get; set; }
+	public List<WeaponDefinition> Gadgets { get; set; }
+
+	protected override void PostLoad()
+	{
+		base.PostLoad();
+
+		Log.Info( $"Registering loadout ({ResourcePath})" );
+
+		if ( !All.Contains( this ) )
+			All.Add( this );
+	}
+
+	/// <summary>
+	/// Fetch loadouts from a specified tag
+	/// </summary>
+	/// <param name="tag"></param>
+	/// <returns></returns>
+	public static IEnumerable<Loadout> WithTag( string tag )
+	{
+		return All.Where( x => x.Tags.Contains( tag ) );
+	}
+
+	/// <summary>
+	/// Applies the loadout onto a player
+	/// </summary>
+	/// <param name="player"></param>
+	public void Give( GunfightPlayer player )
+	{
+		if ( PrimaryWeapon != null )
+			player.GiveWeapon( PrimaryWeapon, true );
+		if ( SecondaryWeapon != null )
+			player.GiveWeapon( SecondaryWeapon );
+		if ( MeleeWeapon != null )
+			player.GiveWeapon( MeleeWeapon );
+	}
+
+	public override string ToString() =>  $"GunfightLoadout[{ResourceName}]";
+}

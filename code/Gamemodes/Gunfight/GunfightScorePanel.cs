31f705910e212ef6bba9a3c0d096f9d5d1093bb2
Tony <devultj@gmail.com>
2022-09-27 14:42:10 -0400

    Redone score panel style

--- code/Gamemodes/Gunfight/GunfightScorePanel.cs
+++ code/Gamemodes/Gunfight/GunfightScorePanel.cs
@@ -6,47 +6,47 @@ namespace Facepunch.Gunfight;
 public partial class GunfightScorePanel : Panel
 {
 	// @ref
-	public Panel BoxLayout { get; set; }
+	public Panel FriendlyBoxes { get; set; }
+	// @ref
+	public Panel EnemyBoxes { get; set; }
+	
 	// @text
-	public string GameState => (GamemodeSystem.Current as GunfightGamemode)?.GetGameStateLabel() ?? "";
+	public string FriendlyScore { get; set; } = "0";
+	// @text
+	public string EnemyScore { get; set; } = "0";
 	// @text
 	public string TimeLabel => (GamemodeSystem.Current as GunfightGamemode)?.GetTimeLeftLabel() ?? "00:00";
-	// @text
-	public string FriendlyCount => $"{TeamSystem.MyTeam.AlivePlayers().Count()}";
-	public string EnemyCount => $"{TeamSystem.MyTeam.GetOpponent().AlivePlayers().Count()}";
 
 	protected List<Panel> Boxes { get; set; } = new();
-
-	public int BoxAmount => ( GunfightGame.Current.Scores.MaximumScore * 2 ) - 1;
+	protected int BoxAmount => GunfightGame.Current.Scores.MaximumScore;
 
 	[Event( "gunfight.scores.changed" )]
 	public void Update()
-	{
-		BoxLayout.DeleteChildren( true );
+	{	
 		Boxes.Clear();
 
-		var myScore = GunfightGame.Current.Scores.GetScore( TeamSystem.MyTeam );
-		var enemyScore = GunfightGame.Current.Scores.GetScore( TeamSystem.GetEnemyTeam( TeamSystem.MyTeam ) );
+		UpdateScores( TeamSystem.MyTeam );
+		UpdateScores( TeamSystem.MyTeam.GetOpponent() );
+
+		FriendlyScore = $"{GunfightGame.Current.Scores.GetScore( TeamSystem.MyTeam )}";
+		EnemyScore = $"{GunfightGame.Current.Scores.GetScore( TeamSystem.MyTeam.GetOpponent() )}";
+	}
+
+	protected void UpdateScores( Team team )
+	{
+		var pnl = team == TeamSystem.MyTeam ? FriendlyBoxes : EnemyBoxes;
+		pnl.DeleteChildren( true );
+
+		var score = GunfightGame.Current.Scores.GetScore( team );
 
 		for ( int i = 0; i < BoxAmount; i++ )
 		{
-			var pnl = BoxLayout.AddChild<Panel>( "box" );
-			if ( i == BoxAmount / 2 )
-				pnl.AddClass( "winning" );
+			var box = pnl.AddChild<Panel>( "box" );
 
-			Boxes.Add( pnl );
-		}
-
-		for ( int i = 0; i < myScore; i++ )
-		{
-			var box = Boxes[i];
-			box.SetClass( "friendly", true );
-		}
-
-		for ( int i = BoxAmount - 1; i > BoxAmount - 1 - enemyScore; i-- )
-		{
-			var box = Boxes[i];
-			box.SetClass( "enemy", true );
+			if ( score > i )
+				box.SetClass( "won", true );
+			
+			Boxes.Add( box );
 		}
 	}
 

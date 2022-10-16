5c55e1b5eefd7663c5b13205853cea8a768af230
Tony <devultj@gmail.com>
2022-10-16 11:15:04 -0400

    Changed loadout defs, added attachments to loadout panel https://files.facepunch.com/devultj/1b1611b1/sbox-dev_y4Pvo00hLF.png

--- code/UI/LoadoutPanel/LoadoutPanel.cs
+++ code/UI/LoadoutPanel/LoadoutPanel.cs
@@ -38,8 +38,11 @@ public partial class LoadoutPanel : Panel
 
 		MainPanel.DeleteChildren( true );
 
-		AddEntry( gamemode.CurrentLoadout.PrimaryWeapon );
-		AddEntry( gamemode.CurrentLoadout.SecondaryWeapon );
+		if ( gamemode.CurrentLoadout.PrimaryWeapon.Definition != null )
+			AddEntry( gamemode.CurrentLoadout.PrimaryWeapon );
+
+		if ( gamemode.CurrentLoadout.SecondaryWeapon.Definition != null )
+			AddEntry( gamemode.CurrentLoadout.SecondaryWeapon );
 	}
 
 	protected void AddEntry( LoadoutSlot slot )
@@ -47,6 +50,24 @@ public partial class LoadoutPanel : Panel
 		var pnl = MainPanel.Add.Panel( "entry" );
 		pnl.AddChild<Image>( "icon" ).SetTexture( slot.Definition.Icon );
 		pnl.AddChild<Label>( "name" ).Text = slot.Definition.WeaponName;
+
+		if ( slot.Attachments != null && slot.Attachments.Count > 0  )
+		{
+			pnl.AddClass( "has-attachments" );
+			var attachmentContainer = pnl.Add.Panel( "attachments" );
+
+			var title = attachmentContainer.AddChild<Label>( "title" );
+			title.Text = "settings";
+
+			foreach ( var attachment in slot.Attachments )
+			{
+				var library = TypeLibrary.GetDescription( attachment );
+				if ( library is null ) continue;
+
+				var attachmentPanel = attachmentContainer.Add.Panel( "attachment" );
+				attachmentPanel.AddChild<Label>( "name" ).Text = library.Title;
+			}
+		}
 	}
 
 	TimeSince TimeSinceCreated = 0;

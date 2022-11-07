a8787a9e810d22b2ce9a8dbdeeda74c6790ef936
Tony <devultj@gmail.com>
2022-11-07 12:06:01 -0500

    Add experience hints, add kill confirm / deny awards

--- code/Progression/Progression.cs
+++ code/Progression/Progression.cs
@@ -4,7 +4,14 @@ public partial class Progression
 {
     public static void GiveXP( Client cl, int amount, string reason )
     {
-        // TODO - Do something with this
+        UI.ExperienceHints.RpcSend( To.Single( cl ), amount, reason );
+    }
+
+    public static void GiveScore( Client cl, int amount )
+    {
+        if ( amount < 1 ) return;
+
+        cl.AddInt( "score", amount );
     }
 
     public static void GiveAward( Client cl, string awardTitle )
@@ -15,9 +22,15 @@ public partial class Progression
 
         award?.Invoke( null, new[] { cl.Pawn as GunfightPlayer } );
 
+        var attr = award.GetCustomAttribute<AwardAttribute>();
+        
         Log.Info( $"Progression: Given award '{award.Title}' to {cl.Name}");
 
+        GiveScore( cl, attr.PointsGiven );
 		RpcGiveAward( To.Single( cl ), award.Title );
+
+        if ( attr.ShareXP )
+            GiveXP( cl, attr.PointsGiven, attr.Description );
     }
     
     [ClientRpc]

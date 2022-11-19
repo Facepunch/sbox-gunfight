81f200b6f84d604d42ada16e885f64d3e76805fc
Tony <devultj@gmail.com>
2022-11-19 17:22:03 +0000

    When Team is assigned on TeamComponent, give its Entity a tag to match

--- code/TeamSystem/TeamComponent.cs
+++ code/TeamSystem/TeamComponent.cs
@@ -2,5 +2,23 @@ namespace Facepunch.Gunfight;
 
 public partial class TeamComponent : EntityComponent
 {
-	[Net] public Team Team { get; set; } = Team.Unassigned;
+	[Net] private Team team { get; set; }
+	
+	public Team Team
+	{
+		get => team;
+		set
+		{
+			var previous = team;
+			team = value;
+
+			SetupTags( Entity is Client cl ? cl.Pawn : Entity, previous, team );
+		}
+	}
+
+	public void SetupTags( Entity ent, Team before, Team after )
+	{
+		ent.Tags.Remove( before.GetTag() );
+		ent.Tags.Add( after.GetTag() );
+	}
 }

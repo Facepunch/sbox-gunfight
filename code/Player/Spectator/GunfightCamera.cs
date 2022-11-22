86ebe32dacafb87d66f598058492b1a79e7ba06b
Tony <devultj@gmail.com>
2022-11-22 23:24:26 +0000

    Added gunfight_thirdperson

--- code/Player/Spectator/GunfightCamera.cs
+++ code/Player/Spectator/GunfightCamera.cs
@@ -20,9 +20,32 @@ internal class GunfightCamera : CameraMode
 	public static bool IsSpectator => Target.IsValid() && !Target.IsLocalPawn;
 	public static bool IsLocal => !IsSpectator;
 
+	public static bool IsThirdPerson { get; set; } = false;
+
 	public virtual IEnumerable<GunfightPlayer> GetPlayers()
 	{
 		return Entity.All.OfType<GunfightPlayer>().Where( x => x.LifeState == LifeState.Alive );
+	}
+
+	public float CurrentDistance { get; set; } = 0f;
+	public float CameraDistance => 60f;
+	public float RightOffset => 4f;
+	public float CameraChangeSpeed { get; set; } = 10f;
+
+	private void UpdateDistance()
+	{
+		var player = Target;
+
+		CurrentDistance = CurrentDistance.LerpTo( IsThirdPerson ? CameraDistance : 0, Time.Delta * CameraChangeSpeed );
+
+		if ( CurrentDistance < 8 )
+		{
+			Viewer = player;
+		}
+		else
+		{
+			Viewer = null;
+		}
 	}
 
 	public override void Update()
@@ -34,13 +57,25 @@ internal class GunfightCamera : CameraMode
 		if ( !target.IsValid() )
 			return;
 
-		Position = target.EyePosition;
+		UpdateDistance();
+
+		Position = Target.EyePosition;
+
+		float distance = CurrentDistance * Target.Scale;
+		var targetPos = Position + Input.Rotation.Right * ((Target.CollisionBounds.Maxs.x + RightOffset) * Target.Scale) * (CurrentDistance / CameraDistance);
+		targetPos += Input.Rotation.Forward * -distance;
+
+		Position = targetPos;
 
 		if ( IsLocal )
 			Rotation = target.EyeRotation;
 		else
 			Rotation = Rotation.Slerp( Rotation, target.EyeRotation, Time.Delta * 20f );
 
-		Viewer = target;
+		Sound.Listener = new()
+		{
+			Position = Target.EyePosition,
+			Rotation = Target.EyeRotation
+		};
 	}
 }

142f51843290f3e6ff430794a9032608dbb49944
Tony <devultj@gmail.com>
2022-11-23 01:23:53 +0000

    Added GamemodeSpecificEntity, added static ammo crate entity

---
+++ code/Entities/GamemodeSpecificEntity.cs
@@ -0,0 +1,16 @@
+namespace Facepunch.Gunfight;
+
+public partial class GamemodeSpecificEntity : AnimatedEntity
+{
+	[Property] public GamemodeType SupportedGamemodes { get; set; } = GamemodeType.Any;
+
+	[Event.Entity.PostSpawn]
+	protected void EventPostSpawn()
+	{
+		if ( SupportedGamemodes.GetArray().Any( x => x == GamemodeSystem.SelectedGamemode || x is null ) )
+			return;
+
+		Delete();
+		Log.Warning( $"Deleting {this} because gamemode doesn't match" );
+	}
+}
--- code/Entities/GunfightSpawnPoint.cs
+++ code/Entities/GunfightSpawnPoint.cs
@@ -3,8 +3,7 @@ namespace Facepunch.Gunfight;
 [Library( "gunfight_player_start" )]
 [HammerEntity, EditorModel( "models/editor/playerstart.vmdl", FixedBounds = true )]
 [Title( "Gunfight: Spawn Point" ), Category( "Setup" ), Icon( "place" )]
-public partial class GunfightSpawnPoint : SpawnPoint
+public partial class GunfightSpawnPoint : GamemodeSpecificEntity
 {
 	[Property] public Team Team { get; set; } = Team.Unassigned;
-	[Property] public GamemodeType SupportedGamemodes { get; set; } = GamemodeType.Any;
 }

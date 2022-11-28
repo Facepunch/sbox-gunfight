ff79bcb55f955ad2900462b15ac0b4756c95da31
Tony <devultj@gmail.com>
2022-11-28 23:52:56 +0000

    Add GunfightSpawnVolume.IsBase, show "Friendly|Enemy Base" on deployment screen if set to true.

--- code/Entities/GunfightSpawnVolume.cs
+++ code/Entities/GunfightSpawnVolume.cs
@@ -8,8 +8,9 @@ public partial class GunfightSpawnVolume : BaseTrigger, ISpawnPoint
 	[Property] public Team Team { get; set; } = Team.Unassigned;
 	[Property] public string PointName { get; set; } = "H";
 	[Property] public string NiceName { get; set; } = "Base";
+	[Property] public bool IsBase { get; set; } = true;
 
-	string ISpawnPoint.GetIdentity() => NiceName;
+	string ISpawnPoint.GetIdentity() => (IsBase ? $"{(TeamSystem.IsFriendly( Team, TeamSystem.MyTeam ) ? "Friendly" : "Enemy")} Base" : NiceName );
 	int ISpawnPoint.GetSpawnPriority() => 1;
 	
 	public Vector3 GetRandomPoint()

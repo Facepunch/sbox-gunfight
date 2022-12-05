e4d607f3dcff77bcc124e0285db782c411e2ffb4
Tony <devultj@gmail.com>
2022-12-05 18:16:32 +0000

    Work in progress UI2 Crosshair replacement

---
+++ code/UI/Crosshair/Crosshair.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox.UI;
+using System.Threading;
+
+namespace Facepunch.Gunfight.UI;
+
+public class Crosshair : Panel
+{
+	public static Crosshair Instance;
+
+	public Crosshair()
+	{
+		Instance = this;
+
+		for ( int i = 0; i < 5; i++ )
+		{
+			var p = Add.Panel( "element" );
+			p.AddClass( $"el{i}" );
+		}
+
+		Style.PixelSnap = 0;
+	}
+
+	public override void Tick()
+	{
+		var wpn = GunfightCamera.Target?.CurrentWeapon;
+		if ( !wpn.IsValid() )
+		{
+			return;
+		}
+
+		SetClass( "fire", wpn.TimeSincePrimaryAttack < 0.2f );
+	}
+}

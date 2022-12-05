f8fa2b20f1b1586d562bc76e9727d8c1aef77ada
Sam Pavlovic <sam@sampavlovic.com>
2022-12-05 09:53:53 +0200

    Much better vaulting mechanic

--- code/WeaponSystem/ViewModel.cs
+++ code/WeaponSystem/ViewModel.cs
@@ -58,6 +58,7 @@ public partial class ViewModel : BaseViewModel
 	float slideLerp = 0;
 	float sideLerp = 0;
 	float speedLerp = 0;
+	float vaultLerp = 0;
 
 	public override void PostCameraSetup( ref CameraSetup camSetup )
 	{
@@ -127,6 +128,7 @@ public partial class ViewModel : BaseViewModel
 		var aim = controller.IsAiming;
 		var crouched = controller?.Duck?.IsActive ?? false;
 		var sliding = controller?.Slide?.IsActive ?? false;
+		var vaulting = controller?.Vault?.IsActive ?? false;
 
 		var avoidanceVal = avoidanceTrace.Hit ? (1f - avoidanceTrace.Fraction) : 0;
 		avoidanceVal *= 1 - ( aimLerp * 0.8f );
@@ -140,6 +142,7 @@ public partial class ViewModel : BaseViewModel
 		LerpTowards( ref slideLerp, ( sliding && !aim ) ? TimeSincePrimaryAttack.Remap( 0, 0.2f, 0, 1 ).Clamp( 0, 1 ) : 0, 7f );
 		LerpTowards( ref airLerp, ( isGrounded ? 0 : 1 ) * ( 1 - aimLerp ), 10f );
 		LerpTowards( ref speedLerp, ( aim || sliding || sprint ) ? 0.0f : speed, 10f );
+		LerpTowards( ref vaultLerp, ( vaulting ) ? 1.0f : 0.0f , 10f );
 
 		var leftAmt = left.WithZ( 0 ).Normal.Dot( Owner.Velocity.Normal );
 		LerpTowards( ref sideLerp, leftAmt * ( 1 - aimLerp ), 5f );
@@ -237,6 +240,9 @@ public partial class ViewModel : BaseViewModel
 				rotationOffsetTarget *= Rotation.From( new Angles( -positionScale, 0, -positionScale ) );
 			}
 		}
+
+		// Vaulting
+		rotationOffsetTarget *= Rotation.From( new Angles(40,0,0) * vaultLerp );
 
 		// Sliding
 		rotationOffsetTarget *= Rotation.From( SlideAngleOffset * slideLerp );

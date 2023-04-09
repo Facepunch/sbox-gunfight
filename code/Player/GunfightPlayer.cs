61648a3701d0b82f188e76b787da16217ef30247
Tony Ferguson <devultj@gmail.com>
2023-04-09 16:28:46 +0100

    Fixed NRE on death

--- code/Player/GunfightPlayer.cs
+++ code/Player/GunfightPlayer.cs
@@ -25,7 +25,7 @@ public partial class GunfightPlayer : AnimatedEntity, IHudMarker
 
 	Sound HeartbeatSound { get; set; }
 
-	public override Ray AimRay => new Ray( Position + Controller.EyeLocalPosition, ViewAngles.Forward );
+	public override Ray AimRay => new Ray( Position + ( Controller?.EyeLocalPosition ?? 0 ), ViewAngles.Forward );
 
 	public GunfightCamera PlayerCamera { get; set; } = new();
 

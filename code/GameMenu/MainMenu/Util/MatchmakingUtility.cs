9bd90dd368219b3be52e2ae807274095ba537a5c
Tony Ferguson <devultj@gmail.com>
2023-08-04 16:57:05 +0100

    Refactor matchmaking system to revolve around a cancellation token, can click matchmaking widget to cancel queue

--- code/GameMenu/MainMenu/Util/MatchmakingUtility.cs
+++ code/GameMenu/MainMenu/Util/MatchmakingUtility.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using Sandbox.Menu;
 using Sandbox.Services;
 
@@ -7,6 +8,8 @@ public partial class MatchmakingSystem
 {
 	public static State CurrentState { get; set; } = State.Empty;
 	public static TimeSince TimeSinceSearch { get; set; } = 0;
+	
+	public static CancellationTokenSource CancellationToken = new();
 
 	public static async Task<bool> JoinServer( ServerList.Entry server )
 	{
@@ -42,7 +45,7 @@ public partial class MatchmakingSystem
 		return result;
 	}
 	
-	public static async Task<bool> Search( string mode = null, string[] maps = null, int reservedSlots = 1 )
+	private static async Task<bool> Search( string mode = null, string[] maps = null, int reservedSlots = 1 )
 	{
 		var lastState = CurrentState;
 		CurrentState = State.Searching;
@@ -50,20 +53,75 @@ public partial class MatchmakingSystem
 		if ( lastState != State.NothingAvailable ) TimeSinceSearch = 0;
 
 		var server = await FindServer( mode, maps, reservedSlots );
+		CancellationToken.Token.ThrowIfCancellationRequested();
+
 		if ( server != null )
 		{
 			return await JoinServer( server.Value );
 		}
 		
 		var lobby = await FindLobby( mode, maps, reservedSlots );
+		CancellationToken.Token.ThrowIfCancellationRequested();
 
 		if ( lobby != null )
 		{
 			return await JoinLobby( lobby );
 		}
-
+		
 		CurrentState = State.NothingAvailable;
 		return false;
+	}
+
+	public static void StopMatchmaking()
+	{
+		CancellationToken.Cancel();
+	}
+
+	private static async Task<string> TryMatchmake( string mode = null, string[] maps = null, int reservedSlots = 1 )
+	{
+		while ( CurrentState != State.Found )
+		{
+			if ( TimeSinceSearch > 30 )
+			{
+				CurrentState = State.Found;
+				await Task.Delay( 1000 );
+
+				// Make the lobby
+				var lobby = await Game.Menu.CreateLobbyAsync( 16, "gunfight", true );
+				lobby.Map = Game.Random.FromArray( maps );
+				lobby.SetData( "gunfight-gamemode", mode );
+				lobby.Title = $"{lobby.Owner.Name}'s game";
+
+				CurrentState = State.Empty;
+
+				return "lobby";
+			}
+			
+			var result = await Search( mode, maps, reservedSlots );
+			CancellationToken.Token.ThrowIfCancellationRequested();
+		}
+		
+		return MainMenu.MainMenu.CurrentLobby != null ? "lobby" : "server";
+	}
+	
+	public static async Task<string> Matchmake( string mode = null, string[] maps = null, int reservedSlots = 1 )
+	{
+		TimeSinceSearch = 0;
+		CancellationToken?.Cancel();
+		CancellationToken = new();
+		
+		try
+		{
+			var result = await TryMatchmake( mode, maps, reservedSlots );
+			return result;
+		}
+		catch ( Exception e )
+		{
+			Log.Info( $"Stopped matchmaking for reason {e}" );
+			
+			CurrentState = State.Empty;
+			return null;
+		}
 	}
 
 	public enum State

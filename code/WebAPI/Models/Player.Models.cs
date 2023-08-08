cb451bafc7980ea4bc6d080d22550d459c08e762
Tony Ferguson <devultj@gmail.com>
2023-08-08 17:35:32 +0100

    Add player endpoints

---
+++ code/WebAPI/Models/Player.Models.cs
@@ -0,0 +1,18 @@
+﻿namespace Facepunch.Gunfight.Models;
+
+public class Player : BaseModel
+{
+	public long SteamId { get; set; }
+
+	public ulong Experience { get; set; }
+
+	public Player( long steamId )
+	{
+		SteamId = steamId;
+	}
+
+	public struct UpdateRequest
+	{
+		public ulong Experience { get; set; }
+	}
+}
---
+++ code/WebAPI/Player/WebAPI.Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Text;
+using Facepunch.Gunfight.Models;
+
+namespace Facepunch.Gunfight;
+
+public partial class WebAPI
+{
+	public static class Player
+	{
+		public static async Task<ulong> GiveExperience( ulong amount )
+		{
+			// This may look confusing. But it's intentional.
+			Game.AssertClient();
+			
+			var player = await HttpPut<Models.Player>( "Player/xp", new StringContent( $"{amount}", null, "application/json" ) );
+			
+			// Return new XP
+			return player.Experience;
+		}
+
+		public static async Task<Models.Player> GetPlayer( ulong steamId )
+		{
+			var player = await HttpGet<Models.Player>( $"Player/{steamId}" );
+			return player;
+		}
+		
+		public static async Task<Models.Player> GetLocalPlayer()
+		{
+			Game.AssertClientOrMenu();
+			return await GetPlayer( Convert.ToUInt64( Game.SteamId ) );
+		}
+	}
+}

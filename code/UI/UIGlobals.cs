237c3ed87388035208d9510265158082254e637e
Tony Ferguson <devultj@gmail.com>
2023-08-25 18:13:51 +0100

    Add UIGlobals, UIGlobals.MenuScale and UIGlobals.GameScale. Scale up respective root panels on Steam Deck

---
+++ code/UI/UIGlobals.cs
@@ -0,0 +1,10 @@
+﻿namespace Facepunch.Gunfight.UI;
+
+public partial class UIGlobals
+{
+    [ConVar.Client( "gunfight_menu_scale" )]
+    public static float MenuScale { get; set; } = 1f;
+
+	[ConVar.Client( "gunfight_game_scale" )]
+    public static float GameScale { get; set; } = 0.8f;
+}

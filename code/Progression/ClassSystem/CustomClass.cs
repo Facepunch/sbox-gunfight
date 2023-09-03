4795cbcbd2903c762b079d7fbc7763c4bf881b51
Tony Ferguson <devultj@gmail.com>
2023-09-03 22:04:19 +0100

    Don't allow multiple attachments to be active if they're in the same category

--- code/Progression/ClassSystem/CustomClass.cs
+++ code/Progression/ClassSystem/CustomClass.cs
@@ -4,6 +4,34 @@ public class Weapon
 {
 	public string Name { get; set; } = "invalid_weapon";
 	public List<string> Attachments { get; set; } = new();
+
+	/// <summary>
+	/// Set an attachment on/off
+	/// </summary>
+	/// <param name="attachment"></param>
+	/// <param name="active"></param>
+	public void SetAttachment( WeaponAttachment attachment, bool active )
+	{
+		if ( !active )
+		{
+			Attachments.Remove( attachment.Identifier );
+		}
+		else
+		{
+			Attachments.RemoveAll( x => WeaponAttachment.Get( x ).Category == attachment.Category );
+			Attachments.Add( attachment.Identifier );
+		}
+	}
+
+	/// <summary>
+	/// Do we have an attachment?
+	/// </summary>
+	/// <param name="attachment"></param>
+	/// <returns></returns>
+	public bool HasAttachment( WeaponAttachment attachment )
+	{
+		return Attachments.Any( x => x == attachment.Identifier );
+	}
 }
 
 public partial class CustomClass

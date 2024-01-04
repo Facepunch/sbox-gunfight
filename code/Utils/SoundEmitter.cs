8e133f7abb5747068cf208eb1641a59084e823c5
Tony Ferguson <devultj@gmail.com>
2024-01-04 10:18:50 +0000

    Fix error spam from sound emitter

--- code/Utils/SoundEmitter.cs
+++ code/Utils/SoundEmitter.cs
@@ -33,6 +33,8 @@ public sealed class SoundEmitter : Component
 
 	protected override void OnUpdate()
 	{
+		if ( handle is null ) return;
+
 		// If we stopped playing, kill the game object
 		if ( handle.IsStopped )
 		{

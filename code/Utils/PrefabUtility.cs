570ac3814b47233951c8447e842c1e2b22768c78
Tony Ferguson <devultj@gmail.com>
2024-03-23 17:29:56 +0000

    GameManager -> Game, removed some useless code

--- code/Utils/PrefabUtility.cs
+++ code/Utils/PrefabUtility.cs
@@ -5,22 +5,12 @@ namespace Gunfight;
 
 public static class PrefabUtility
 {
+	// TODO: Kill this shit off
 	public static GameObject CreateGameObject( PrefabFile prefabFile, GameObject parent = null )
 	{
-		Assert.NotNull( GameManager.ActiveScene, "No Active Scene" );
+		Assert.NotNull( Game.ActiveScene, "No Active Scene" );
 
-		JsonObject json = prefabFile.RootObject;
-
-		//if ( template is PrefabScene prefabScene && prefabScene.Source is PrefabFile prefabFile )
-		//{
-		//	json = prefabFile.RootObject;
-		//}
-		//else
-		//{
-		//	json = template.Serialize();
-		//}
-
-		MakeGameObjectsUnique( json );
+		var json = prefabFile.RootObject;
 
 		var go = new GameObject();
 		go.Deserialize( json );
@@ -32,43 +22,5 @@ public static class PrefabUtility
 		}
 
 		return go;
-	}
-
-	/// <summary>
-	/// Find all "Id" guids, and replace them with new Guids.
-	/// This is used to make GameObject serializations unique, so when
-	/// you duplicate stuff, it copies over uniquely and keeps associations.
-	/// </summary>
-	public static void MakeGameObjectsUnique( JsonObject json )
-	{
-		Dictionary<Guid, Guid> translate = new();
-
-		//
-		// Find all guids with "Id" as their name. Add them to translate 
-		// with a new target value.
-		//
-		Sandbox.Json.WalkJsonTree( json, ( k, v ) =>
-		{
-			if ( k != "Id" ) return v;
-
-			if ( v.TryGetValue<Guid>( out var guid ) )
-			{
-				translate[guid] = Guid.NewGuid();
-			}
-
-			return v;
-		} );
-
-		//
-		// Find every guid and translate them, but only if they're in our
-		// guid dictionary.
-		//
-		Sandbox.Json.WalkJsonTree( json, ( k, v ) =>
-		{
-			if ( !v.TryGetValue<Guid>( out var guid ) ) return v;
-			if ( !translate.TryGetValue( guid, out var updatedGuid ) ) return v;
-
-			return updatedGuid;
-		} );
 	}
 }
